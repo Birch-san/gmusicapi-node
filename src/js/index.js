90b932b6de534e970a0112690ae76b0a4c7fcb97
Alex Birch <vengeance.mx@gmail.com>
2015-11-15 16:11:32 +0000

    theoretically attempt to call keychain, but it's complainin'

--- src/js/index.js
+++ src/js/index.js
@@ -10,8 +10,8 @@ function init(options) {
 			usekeychain: false,
 			keychainSpec: {
 				account: undefined,
-				service: undefined,
-				type: undefined
+				service: 'https://accounts.google.com/ServiceLogin',
+				type: 'internet'
 			}
 		},
 		pyshellOptions: {},
@@ -50,10 +50,22 @@ module.exports = function(options) {
 	}
 
 	if (globalState.credentials.usekeychain) {
-		return new keychain.getPassword({
+		if (!globalState.credentials.keychainSpec.account) {
+			throw new Error("No `account` specified in `options.credentials.keychainSpec`!");
+		}
+		if (!globalState.credentials.keychainSpec.service) {
+			throw new Error("No `service` specified in `options.credentials.keychainSpec`!");
+		}
+		if (!globalState.credentials.keychainSpec.type) {
+			throw new Error("No `type` specified in `options.credentials.keychainSpec`!");
+		}
 
-		}, function(err, pass) {
-
+		return new keychain.getPassword(globalState.credentials.keychainSpec, function(err, pass) {
+			if (err) {
+				console.error(err);
+			} else {
+				console.log(pass);
+			}
 		});
 	}
 
