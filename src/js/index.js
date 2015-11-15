f97d4b7635f558ef8b2f7eb218e87569d5f887d7
Alex Birch <vengeance.mx@gmail.com>
2015-11-15 16:11:32 +0000

    refactor entry point

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,22 @@
-module.exports = require('./bindings');
+var _ = require('lodash');
+var globalState = require('./globalState');
 
+function init(options) {
+	_.merge(globalState, {
+		logLevel: 'info'
+	}, options);
+}
+
+function checkSanity() {
+	require('./checks/sanityCheck');
+}
+
+module.exports = function(options) {
+	init(options || {});
+
+	if (!globalState.skipSanityChecks){
+		checkSanity();
+	}
+
+	return require('./bindings');
+};
