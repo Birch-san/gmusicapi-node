e0f50a506beca6f840747828ff51d062ea471439
Alex Birch <vengeance.mx@gmail.com>
2015-11-15 16:11:39 +0000

    Remove unused lines

--- src/js/pyshellWrapper.js
+++ src/js/pyshellWrapper.js
@@ -1,8 +1,4 @@
-var PythonShell = require('python-shell');
-var _ = require('lodash');
-var pathRelToCwd = require('./pathRelToCwd');
 var whenError = require('./whenError');
-var globalState = require('./globalState');
 
 var pyshellWrapperHardMode = require('./pyshellWrapperHardMode');
 
